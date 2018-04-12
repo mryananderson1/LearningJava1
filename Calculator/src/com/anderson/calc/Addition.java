dfa9089402b446feea6354ce74fcce91fa6c11ad
mryananderson1 <matthanderson@deloitte.com>
2018-04-12 12:48:32 -0400

    changing addition message

---
+++ Calculator/src/com/anderson/calc/Addition.java
@@ -0,0 +1,11 @@
+package com.anderson.calc;
+
+public class Addition 
+{
+	public int addNumbers(int integerOne, int integerTwo)
+	{
+		int additionResult = integerOne + integerTwo;
+		return additionResult;
+	}
+	
+}
---
+++ Calculator/src/com/anderson/calc/RunCalculator.java
@@ -0,0 +1,13 @@
+package com.anderson.calc;
+
+public class RunCalculator 
+{
+	public static void main (String[] args)
+	{
+		Addition addition = new Addition();
+		int integerOne = 1;
+		int integerTwo = 2;
+		addition.addNumbers(integerOne, integerTwo);
+	}
+
+}
